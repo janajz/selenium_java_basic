7ea25ba4b4831f12e94081d3e060805095134b99
Jana Jermakova-Zaikovska <102352936+janajz@users.noreply.github.com>
2022-04-06 00:44:57 +0300

    Commit SampleTasks - 7 and 9

--- src/selenium/sample/Sample7Task.java
+++ src/selenium/sample/Sample7Task.java
@@ -4,12 +4,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -45,6 +48,24 @@ public class Sample7Task {
 //        tick  "Option 3"
 //        click result
 //        check that text 'You selected value(s): Option 2, Option 3' is being displayed
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
+        for (WebElement checkBox : checkBoxes) {
+            assertFalse(checkBox.isSelected()); // checkboxes are NOT selected
+        }
+        WebElement Option1 = driver.findElement(By.cssSelector(".w3-check[value='Option 1'][type='checkbox']"));
+        WebElement Option2 = driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='checkbox']"));
+        WebElement Option3 = driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='checkbox']"));
+        //Click Option2
+        Option2.click();
+        assertFalse(Option1.isSelected());
+        assertTrue(Option2.isSelected());
+        assertFalse(Option3.isSelected());
+        //Click Option3
+        Option3.click();
+        //Click Result button
+        driver.findElement(By.id("result_button_checkbox")).click();
+        assertEquals("You selected value(s): Option 2, Option 3", driver.findElement(By.id("result_checkbox")).getText());
+        Thread.sleep(2000);
     }
 
 
@@ -58,6 +79,27 @@ public class Sample7Task {
 //        check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
 //        click result
 //        check that 'You selected option: Option 1' text is being displayed
+        List<WebElement> radioBoxes = driver.findElements(By.cssSelector(".w3-check[type='radio']"));
+        for (WebElement checkBox : radioBoxes) {
+            assertFalse(checkBox.isSelected()); // checkboxes are NOT selected
+        }
+        WebElement Option1 = driver.findElement(By.cssSelector(".w3-check[value='Option 1'][type='radio']"));
+        WebElement Option2 = driver.findElement(By.cssSelector(".w3-check[value='Option 2'][type='radio']"));
+        WebElement Option3 = driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='radio']"));
+        //Click Option3
+        Option3.click();
+        assertFalse(Option1.isSelected());
+        assertFalse(Option2.isSelected());
+        assertTrue(Option3.isSelected());
+        //Click Option1
+        Option1.click();
+        assertTrue(Option1.isSelected());
+        assertFalse(Option2.isSelected());
+        assertFalse(Option3.isSelected());
+        //Click Result button
+        driver.findElement(By.id("result_button_ratio")).click();
+        assertEquals("You selected option: Option 1", driver.findElement(By.id("result_radio")).getText());
+        Thread.sleep(2000);
     }
 
     @Test
@@ -68,6 +110,15 @@ public class Sample7Task {
 //        check that selected option is "Option 2"
 //        click result
 //        check that 'You selected option: Option 2' text is being displayed
+        Select dropdown = new Select(driver.findElement(By.id("vfb-12")));
+        dropdown.selectByValue("value3");
+        assertEquals("Option 3", dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByValue("value2");
+        assertEquals("Option 2", dropdown.getFirstSelectedOption().getText());
+        //Click Result button
+        driver.findElement(By.id("result_button_select")).click();
+        assertEquals("You selected option: Option 2", driver.findElement(By.id("result_select")).getText());
+        Thread.sleep(2000);
     }
 
     @Test
@@ -75,6 +126,25 @@ public class Sample7Task {
 //         TODO:
 //        enter date '4 of July 2007' using calendar widget
 //        check that correct date is added
+        String dateToEnter = "08/01/2007";
+        String targetDate = "07/04/2007";
+
+        //Enter initial date in Date box
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        dateBox.click();
+        dateBox.sendKeys(dateToEnter);
+
+        //Go to previous month in Calendar widget
+        WebElement dateWidget = driver.findElement(By.id("ui-datepicker-div"));
+        dateWidget.findElement(By.className("ui-datepicker-prev")).click();
+        //select date 4
+        dateWidget.findElement(By.xpath("//a[text()='4']")).click();
+        assertEquals(targetDate, dateBox.getAttribute("value"));
+        Thread.sleep(3000);
+        //Click Result button
+        driver.findElement(By.id("result_button_date")).click();
+        assertEquals("You entered date: 07/04/2007", driver.findElement(By.id("result_date")).getText());
+        Thread.sleep(2000);
     }
 
     @Test
@@ -82,5 +152,21 @@ public class Sample7Task {
 //         TODO:
 //        enter date '2 of May 1959' using text
 //        check that correct date is added
+        String dateToEnter = "05/02/1959";
+
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        assertEquals("", dateBox.getAttribute("value"));
+
+        Thread.sleep(2000);
+        dateBox.clear();
+        dateBox.sendKeys(dateToEnter);
+        assertEquals(dateToEnter, dateBox.getAttribute("value"));
+        Thread.sleep(3000);
+
+        //Click Result button
+        dateBox.sendKeys(Keys.ESCAPE);
+        driver.findElement(By.id("result_button_date")).click();
+        assertEquals("You entered date: 05/02/1959", driver.findElement(By.id("result_date")).getText());
+        Thread.sleep(2000);
     }
 }
--- src/selenium/sample/Sample9Task.java
+++ src/selenium/sample/Sample9Task.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,11 +13,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Sample9Task {
     WebDriver driver;
+    private static WebDriverWait wait;
+    static long startTime;
 
     @Before
     public void openPage() {
@@ -24,6 +27,9 @@ public class Sample9Task {
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
         driver.get("https://kristinek.github.io/site/examples/loading_color");
+        wait = (WebDriverWait) new WebDriverWait(driver, 10).ignoring(StaleElementReferenceException.class);
+
+
     }
 
     @After
@@ -40,6 +46,18 @@ public class Sample9Task {
 //         * 3) check that both button
 //         * and loading text is not seen,
 //         * success is seen instead "Green Loaded"
+
+
+        System.out.println("Start loading green");
+        driver.findElement(By.id("start_green")).click();
+        Thread.sleep(2000);
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+        Thread.sleep(5000);
+        assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+
     }
 
     @Test
@@ -51,30 +69,87 @@ public class Sample9Task {
 //         * 3) check that both button
 //         * and loading text is not seen,
 //         * success is seen instead "Green Loaded"
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        System.out.println("Start loading green");
+        driver.findElement(By.id("start_green")).click();
+
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+
+        assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+
+
     }
+
 
     @Test
     public void loadGreenExplicitWait() throws Exception {
 //         TODO:
 //         * 1) click on start loading green button
-//         * 2) check that button does not appear,
-//         * but loading text is seen instead   "Loading green..."
+//         * 2) check that button does not appear, but loading text is seen instead "Loading green..."
 //         * 3) check that both button
 //         * and loading text is not seen,
 //         * success is seen instead "Green Loaded"
+
+        System.out.println("Start loading green");
+        driver.findElement(By.id("start_green")).click();
+
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"finish_green\"]")));
+        assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
     }
 
     @Test
     public void loadGreenAndBlueBonus() {
         /* TODO:
-         * 0) wait until button to load green and blue appears
-         * 1) click on start loading green and blue button
+         * 0) wait until button to load 2 button appears
+         *
+         * 1) click on start loading 2 button
+         *
          * 2) check that button does not appear, but loading text is seen instead for green
+         *
          * 3) check that button does not appear, but loading text is seen instead for green and blue
-         * 4) check that button and loading green does not appear,
-         * 		but loading text is seen instead for blue and success for green is seen
+         *
+         * 4) check that button and loading green does not appear, but loading text is seen instead for blue and success for green is seen
+         *
          * 5) check that both button and loading text is not seen, success is seen instead
          */
+
+        System.out.println("Start loading green");
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id=\"start_green_and_blue\"]"))));
+
+        System.out.println("Start loading green and blue");
+        driver.findElement(By.id("start_green_and_blue")).click();
+
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green_without_blue")).isDisplayed());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"loading_green_with_blue\"]")));
+
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green_with_blue")).isDisplayed());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"loading_blue_without_green\"]")));
+
+        assertTrue(driver.findElement(By.id("loading_blue_without_green")).isDisplayed());
+
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"finish_green_and_blue\"]")));
+
+        assertTrue(driver.findElement(By.id("finish_green_and_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green_without_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green_with_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_blue_without_green")).isDisplayed());
+
+
     }
 
 }
